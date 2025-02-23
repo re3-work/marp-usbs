892f40f0b0c40aee26cf93ab4a1af71f7b68f98e
Julian Prester <hi@julianprester.com>
2025-02-23 15:04:57 +1100

    Add markdown-it-variable

--- assets/engine.js
+++ assets/engine.js
@@ -2,11 +2,13 @@ const markdownItMark = require('markdown-it-mark')
 const markdownItContainer = require('markdown-it-container')
 const markdownItAdmon = require('markdown-it-admon')
 const markdownItInclude = require('markdown-it-include')
+const markdownItVariable = require('markdown-it-variable')
 
 module.exports = ({ marp}) => marp
   .use(markdownItMark)
   .use(markdownItAdmon)
   .use(markdownItInclude, '/home/marp/app')
+  .use(markdownItVariable)
   .use(markdownItContainer, "sources")
   .use(markdownItContainer, "columns", {
     render: function (tokens, idx) {
